63c65c5350386a3e99e54d9567dc9fee0a48c0f6
Jose Ramon Hurtado Mije <jhurtadomije@gmail.com>
2026-03-27 10:33:08 +0100

    varios

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 
-const mysql = require("mysql2/promise");
+/*const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -7,6 +7,26 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
+});
+
+module.exports = pool;*/
+
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 4000,
+
+  ssl: {
+    rejectUnauthorized: true,
+  },
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 module.exports = pool;
