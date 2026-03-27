43033da98dacd87ea4a93d34f6a2a4ee99f7302b
Jose Ramon Hurtado Mije <jhurtadomije@gmail.com>
2026-03-27 19:19:11 +0100

    cambio database

--- backend/config/db.js
+++ backend/config/db.js
@@ -14,11 +14,11 @@ module.exports = pool;*/
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 4000,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 
   ssl: {
     rejectUnauthorized: false,
